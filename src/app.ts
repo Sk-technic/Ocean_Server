c51ad6b492fee8390bbbd6320c820ceefb649e3d
dev-shubhamsk <code.dev.sk@gmail.com>
2026-01-07 19:24:56 +0530

    update origins

--- src/app.ts
+++ src/app.ts
@@ -12,7 +12,7 @@ import passport from './config/passport'
 const app: Application = express();
 
 app.use(helmet());
-app.use(cors({ origin:process.env.CLIENT_URI, credentials: true }));
+app.use(cors({ origin: [`${process.env.CLIENT_URI}`,'http://localhost:7000'], credentials: true }));
 
 if (process.env.NODE_ENV === 'development') {
   app.use(morgan('dev'));
